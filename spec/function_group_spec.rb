ce978cefa8032c408feec741011e4eb106f752e1
Thomas Peikert <t.s.peikert@gmail.com>
2018-11-09 09:17:57 +0100

    add logging spec for FunctionGroup

--- spec/function_group_spec.rb
+++ spec/function_group_spec.rb
@@ -5,20 +5,36 @@ RSpec.describe FunctionGroup do
   end
 
   describe '#execute' do
-    it 'executes all functions and returns the results' do
-      functions = []
+    before do
+      @functions = []
       (1..3).each do |n|
-        functions << Function.new("some_function_#{n}", '', '', @logger)
+        @functions << Function.new("some_function_#{n}", '', '', @logger)
       end
+      @functions.each do |function|
+        allow(function).to receive(:execute)
+      end
+    end
 
+    it 'executes all functions and returns the results' do
       data = 7
-      fg = FunctionGroup.new(functions, @logger)
-      functions.each do |function|
+      fg = FunctionGroup.new(@functions, @logger)
+      @functions.each do |function|
         expect(function).to receive(:execute).with(data).
           and_return(function.function_name)
       end
 
-      expect(fg.execute(data)).to eq functions.map(&:function_name)
+      expect(fg.execute(data)).to eq @functions.map(&:function_name)
+    end
+
+    it 'logs output' do
+      fg = FunctionGroup.new(@functions, @logger)
+      expect(@logger).to receive(:info).with(FunctionGroup::START_SEPARATOR)
+      expect(@logger).to receive(:info).with(
+        /#{@functions.map(&:function_name).join(', ')}/
+      )
+      expect(@logger).to receive(:info).with(FunctionGroup::END_SEPARATOR)
+
+      fg.execute(12)
     end
   end
 end
