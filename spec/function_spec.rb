cd750f2a784323d22c117817575e6ce45fcd4091
Thomas Peikert <t.s.peikert@gmail.com>
2018-11-11 16:46:57 +0100

    add specs for Function with POST

--- spec/function_spec.rb
+++ spec/function_spec.rb
@@ -67,10 +67,42 @@ RSpec.describe FunctionGroup do
     end
 
     context 'with http_method POST' do
-      it 'does a POST call' do
+      before do
+        @method = 'POST'
+        @response = 'hello from function'
+        allow(Net::HTTP).to receive(:new).and_return(http = double)
+        allow(http).to receive(:request).and_return response = double
+        allow(response).to receive(:is_a?).with(Net::HTTPSuccess).and_return true
+        allow(response).to receive(:body).and_return(@response)
 
+        @header = {'Content-Type': 'text/json'}
+        @data = { some: 'request' }
+      end
+
+      it 'makes a POST request' do
+        f = Function.new(@name, @method, 0, @logger)
+        expect(URI).to receive(:parse).with(/#{@name}/).
+          and_return(uri = double)
+        allow(uri).to receive(:host).and_return('some_url')
+        allow(uri).to receive(:port).and_return('8080')
+        allow(uri).to receive(:request_uri).and_return 'something'
+        expect(Net::HTTP::Post).to receive(:new).with('something', @header).
+          and_return(request = double)
+        expect(request).to receive(:body=).with(@data.to_json)
+
+        f.execute(@data)
+      end
+
+      it 'logs output' do
+        f = Function.new(@name, @method, 0, @logger)
+        expect(@logger).to receive(:info).with("Calling #{@name} via #{@method}")
+        expect(@logger).to receive(:debug).with(
+          /^.*?\bhello\b.*?\b#{@response}\b.*?$/m
+        )
+        expect(@logger).to receive(:info).with(Function::SEPARATOR)
+
+        f.execute('hello')
       end
     end
-
   end
 end
